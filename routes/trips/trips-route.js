a8543a777a3f80a0f7a2b4d088390fb0b34b4cab
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2025-05-08 14:37:26 -0400

    Remove trip model

--- routes/trips/trips-route.js
+++ routes/trips/trips-route.js
@@ -1,9 +1,6 @@
 import express from "express";
 const router = express.Router();
 
-// Import trip model
-import Trip from "../../models/trip/trips-model";
-
 router.get("/", (req, res) => {
     res.send("Trip routes")
 })
