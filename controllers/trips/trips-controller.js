ff16cdfb039f182a9eee95237a52d7d8c941b736
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2025-05-08 18:12:28 -0400

    Create get a trip

--- controllers/trips/trips-controller.js
+++ controllers/trips/trips-controller.js
@@ -1,52 +1,63 @@
-import Trip from "../../models/trip/trips-model.js"
+import Trip from "../../models/trip/trips-model.js";
 
 // Get all trips
 async function getTrips(req, res) {
-    try {
-       const trips = await Trip.find({}) 
-       res.status(200).json(trips)
-    } catch (error) {
-        res.status(400).json("")
-    }
+  try {
+    const trips = await Trip.find({});
+    res.status(200).json(trips);
+  } catch (error) {
+    res.status(400).json("");
+  }
 }
 
 // Create a trip
 async function createTrip(req, res) {
-    try {
-        const trip = await Trip.create({
-            ...req.body,
-            imageURL: req.file?.path
-        })
+  try {
+    const trip = await Trip.create({
+      ...req.body,
+      imageURL: req.file?.path,
+    });
 
-        res.status(201).json(trip)
-    } catch (error) {
-        res.status(400).json({error: error.message})
-    }
+    res.status(201).json(trip);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 }
 
 // Update a trip
 async function updateTrip(req, res) {
-    try {
-        const updatedTrip = {
-            ...req.body,
-            // Only spreads if the image is updated
-            ...(req.file?.path && { imageURL: req.file.path })
-        }
-        const trip = await Trip.findByIdAndUpdate(req.params.id, updatedTrip,
-            //Return the update form, before there was a delay
-            { new: true}
-        )
+  try {
+    const updatedTrip = {
+      ...req.body,
+      // Only spreads if the image is updated
+      ...(req.file?.path && { imageURL: req.file.path }),
+    };
+    const trip = await Trip.findByIdAndUpdate(
+      req.params.id,
+      updatedTrip,
+      //Return the update form, before there was a delay
+      { new: true }
+    );
 
-        if(!trip) {
-            res.json("No trip found")
-        }
-
-        res.status(200).json(trip)
-        
-
-    } catch (error) {
-        res.status(400).json({error: error.message})
+    if (!trip) {
+      res.json("No trip found");
     }
+
+    res.status(200).json(trip);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 }
 
-export { getTrips, createTrip, updateTrip }
+// Get a trip by id
+async function getTrip(req, res) {
+  try {
+    const trip = await Trip.findById(req.params.id);
+
+    res.status(200).json(trip);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+}
+
+export { getTrips, createTrip, updateTrip, getTrip };
