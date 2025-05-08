5b3b6b672da2bf74ea8195948fc57cbe8f5fb2b1
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2025-05-08 18:16:14 -0400

    Create get and put methods

--- routes/trips/trips-route.js
+++ routes/trips/trips-route.js
@@ -10,8 +10,14 @@ import * as TripsController from "../../controllers/trips/trips-controller.js"
 // Get all trips
 router.get("/", TripsController.getTrips)
 
+// Get a trip by its id
+router.get("/:id", TripsController.getTrip)
+
 // Create a trip
 // upload.single allows only one image 
 router.post("/", upload.single("image"), TripsController.createTrip)
+
+// Update a trip
+router.put("/:id",upload.single("image"), TripsController.updateTrip)
 
 export default router
