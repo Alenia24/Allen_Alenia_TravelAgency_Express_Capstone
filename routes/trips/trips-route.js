b8acaa0dc27c765ca7b816adf9f7d603d4a91590
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2025-05-08 15:06:58 -0400

    Create get all trips method

--- routes/trips/trips-route.js
+++ routes/trips/trips-route.js
@@ -1,8 +1,10 @@
 import express from "express";
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("Trip routes")
-})
+// Import the controller
+import * as TripsController from "../../controllers/trips/trips-controller.js"
+
+// Get all trips
+router.get("/", TripsController.getTrips)
 
 export default router
