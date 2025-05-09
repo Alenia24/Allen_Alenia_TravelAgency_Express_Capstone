1d5748f64771d90177df0d3d5a7ecd234da3bf14
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2025-05-08 21:03:11 -0400

    Import bookings route

--- server.js
+++ server.js
@@ -6,29 +6,31 @@ const app = express();
 
 const PORT = process.env.PORT || 5050;
 // Import the database connection
-import "./database/database.js"
+import "./database/database.js";
 
 // Import routes
-import tripsRouter from "./routes/trips/trips-route.js"
+import tripsRouter from "./routes/trips/trips-route.js";
+import bookingsRouter from "./routes/bookings/bookings-route.js";
 
 // Enable to use req.body when forms are submitted
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 // Enable to use req.body without a form, eg. from reqbin, postman, etc
-app.use(express.json())
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Welcome to the Travel Agency API.")
-})
+  res.send("Welcome to the Travel Agency API.");
+});
 
 // Enable routes use
 app.use("/trips", tripsRouter);
+app.use("/bookings", bookingsRouter);
 
 //Error Handling Middleware
 app.use((req, res) => {
-    res.status(404).send("Resource Not found.")
-})
+  res.status(404).send("Resource Not found.");
+});
 
 // App.listen
 app.listen(PORT, () => {
-    console.log(`Server running on port: ${PORT}`);
-})
+  console.log(`Server running on port: ${PORT}`);
+});
