2b683cd306f6d1b3a465748ef6f9140cdfa5ccc3
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2025-05-08 21:48:27 -0400

    Create get reviews by trip

--- controllers/reviews/reviews-controller.js
+++ controllers/reviews/reviews-controller.js
@@ -6,7 +6,7 @@ async function getReviews(req, res) {
     const reviews = await Review.find({});
 
     if (!reviews || reviews.length === 0) {
-      return res.status(200).json({ message: "No Reviews Available" });
+      return res.status(200).json({ message: "No Reviews Available." });
     }
 
     res.status(200).json(reviews);
@@ -15,4 +15,19 @@ async function getReviews(req, res) {
   }
 }
 
-export { getReviews }
+// Get all reviews for a trip
+async function getTripReviews(req, res) {
+    try {
+        const reviews = await Review.findById({trip: req.params.id})
+        
+        if(!reviews || reviews.length ===0) {
+            return res.status(200).json({ message: "Trip has No Reviews."})
+        }
+
+        res.status(200).json(reviews)
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+}
+
+export { getReviews, getTripReviews }
