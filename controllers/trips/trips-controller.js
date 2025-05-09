ecabc15f8c2c986ec5493dee9bc9b842ccbe5f1c
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2025-05-08 22:42:53 -0400

    Update create trip to be unique

--- controllers/trips/trips-controller.js
+++ controllers/trips/trips-controller.js
@@ -5,8 +5,8 @@ async function getTrips(req, res) {
   try {
     const trips = await Trip.find({});
 
-    if(!trips || trips.length === 0) {
-        return res.status(200).json({ message: "No Trips Available."})
+    if (!trips || trips.length === 0) {
+      return res.status(200).json({ message: "No Trips Available." });
     }
 
     res.status(200).json(trips);
@@ -18,6 +18,14 @@ async function getTrips(req, res) {
 // Create a trip
 async function createTrip(req, res) {
   try {
+    const existingTrip = await Trip.findOne({
+      title: req.body.title.toLowerCase(),
+    });
+
+    if(existingTrip) {
+      return res.status(400).json({ message: "Trip Title Taken."})
+    }
+
     const trip = await Trip.create({
       ...req.body,
       imageURL: req.file?.path,
@@ -71,17 +79,17 @@ async function getTrip(req, res) {
 
 // Delete a trip by id
 async function deleteTrip(req, res) {
-    try {
-        const trip = await Trip.findByIdAndDelete(req.params.id)
+  try {
+    const trip = await Trip.findByIdAndDelete(req.params.id);
 
-        if (!trip) {
-          return res.status(404).json({ message: "Trip Not Found" });
-        }
-
-        res.status(200).json({ message: "Trip Deleted Successfully."})
-    } catch (error) {
-        res.status(500).json({ error: error.message })
+    if (!trip) {
+      return res.status(404).json({ message: "Trip Not Found" });
     }
+
+    res.status(200).json({ message: "Trip Deleted Successfully." });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
 export { getTrips, createTrip, updateTrip, getTrip, deleteTrip };
