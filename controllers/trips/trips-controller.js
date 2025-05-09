a7d3b551b7639f22eb74fc1060d9da500a6d8298
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2025-05-08 20:21:52 -0400

    Update error messages ans status codes

--- controllers/trips/trips-controller.js
+++ controllers/trips/trips-controller.js
@@ -4,9 +4,14 @@ import Trip from "../../models/trip/trips-model.js";
 async function getTrips(req, res) {
   try {
     const trips = await Trip.find({});
+
+    if(!trips || trips.length === 0) {
+        return res.status(200).json({ message: "No Trips Available."})
+    }
+
     res.status(200).json(trips);
   } catch (error) {
-    res.status(400).json("");
+    res.status(500).json({ error: error.message });
   }
 }
 
@@ -20,7 +25,7 @@ async function createTrip(req, res) {
 
     res.status(201).json(trip);
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res.status(500).json({ error: error.message });
   }
 }
 
@@ -40,12 +45,12 @@ async function updateTrip(req, res) {
     );
 
     if (!trip) {
-      res.json("No trip found");
+      return res.status(404).json({ message: "Trip Not Found" });
     }
 
     res.status(200).json(trip);
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res.status(500).json({ error: error.message });
   }
 }
 
@@ -54,9 +59,13 @@ async function getTrip(req, res) {
   try {
     const trip = await Trip.findById(req.params.id);
 
+    if (!trip) {
+      return res.status(404).json({ message: "Trip Not Found" });
+    }
+    
     res.status(200).json(trip);
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res.status(500).json({ error: error.message });
   }
 }
 
