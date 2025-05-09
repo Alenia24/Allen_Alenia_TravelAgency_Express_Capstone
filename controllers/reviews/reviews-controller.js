436c0491808e1e1ab09770daee0fcbfea6fa15f0
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2025-05-08 21:34:59 -0400

    Create get all reviews

---
+++ controllers/reviews/reviews-controller.js
@@ -0,0 +1,18 @@
+import Review from "../../models/reviews/reviews-model.js";
+
+// Get all reviews
+async function getReviews(req, res) {
+  try {
+    const reviews = await Review.find({});
+
+    if (!reviews || reviews.length === 0) {
+      return res.status(200).json({ message: "No Reviews Available" });
+    }
+
+    res.status(200).json(reviews);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+export { getReviews }
