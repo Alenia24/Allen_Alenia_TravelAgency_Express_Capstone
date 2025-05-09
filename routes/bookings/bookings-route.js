1d67d7efa86c599ace6477f8cb98fcdeb453f3e7
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2025-05-08 21:17:48 -0400

    Create post booking method

--- routes/bookings/bookings-route.js
+++ routes/bookings/bookings-route.js
@@ -1,4 +1,9 @@
 import express from "express";
 const router = express.Router();
 
+import * as BookingsController from "../../controllers/bookings/bookings-controller.js"
+
+// Create a booking
+router.post("/", BookingsController.createBooking)
+
 export default router
