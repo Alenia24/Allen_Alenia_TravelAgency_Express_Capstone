aa4b953a656c1b86336a3a31dd3d4b41b5b6d169
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2025-05-10 11:21:10 -0400

    Create register user method

--- routes/auth/auth-route.js
+++ routes/auth/auth-route.js
@@ -1,4 +1,10 @@
 import express from "express";
 const router = express.Router();
 
+// Import the auth controller
+import * as AuthController from "../../controllers/auth/auth-controller.js";
+
+// Create a user
+router.post("/register", AuthController.createUser);
+
 export default router;
