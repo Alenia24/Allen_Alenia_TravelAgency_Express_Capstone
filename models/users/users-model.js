6ec2b2edcb89e2325cdf99994d302d6726bc42bb
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2025-05-10 10:58:05 -0400

    Create user model

---
+++ models/users/users-model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+// Define the User Schema
+const userSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+      max: 12,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
