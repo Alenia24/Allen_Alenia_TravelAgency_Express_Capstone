301f92686a7c5349d4e8a19ca80620b7d2983314
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2025-05-10 12:09:22 -0400

    Create logout user method

--- routes/auth/auth-route.js
+++ routes/auth/auth-route.js
@@ -10,4 +10,7 @@ router.post("/register", AuthController.createUser);
 // Login a user
 router.post("/login", AuthController.loginUser)
 
+// Logout a user
+router.delete("/logout", AuthController.logoutUser)
+
 export default router;
